54c23453871b24d2438a4d1b10f6ffc2fe750edc
Watsh Rajneesh <rwatsh@gmail.com>
2019-12-31 10:01:24 -0600

    updated with stringutil ref

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sjsucohort6/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("Hello, world."))
 }
